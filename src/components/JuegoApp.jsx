517dce6ceea8a965fe4f972a3bb11f146639f1d4
cristianh <crishernandez08@hotmail.com>
2023-01-20 19:25:29 -0500

    UPDATE: ajustando validacion y logica opciones

--- src/components/JuegoApp.jsx
+++ src/components/JuegoApp.jsx
@@ -1,10 +1,13 @@
-import React, { useState, useEffect } from 'react'
+import React, { useState } from 'react'
 import './JuegoApp.css'
+import papel from '../assets/Papel.png'
+import piedra from '../assets/Piedra.png'
+import tijera from '../assets/Tijera.png'
 export const JuegoApp = () => {
 
     const opciones = ["piedra", "papel", "tijera"]
-    const vidas = []
 
+    const [FinJuego, setFinJuego] = useState(false)
     const [PartidaActual, setPartidaActual] = useState(0)
     const [PuntosJugador, setPuntosJugador] = useState(0)
     const [PuntosComputadora, setPuntosComputadora] = useState(0)
@@ -17,57 +20,70 @@ export const JuegoApp = () => {
 
 
     const onOpcionCheck = (opcion) => {
-        if (PartidaActual != 4) {
-            //actualizamos la seleccion del jugador
-            setJuegadorOpcion(opcion)
-setPartidaActual(PartidaActual + 1)
-            //Generamos aleatoriamente una Opcion para la  computadora
-            const randomOpcionCompuradora = Math.floor(Math.random() * 3)
-            //Pasamos la opcion 
-            const opcionComputadora = opciones[randomOpcionCompuradora]
-            //actualizamos el estado de la computadora
-            setComputadorOpcion(opcionComputadora)
 
-            //Validamos el juego actual
-            if (opcion === opcionComputadora) {
-                setResultado("Empate")
-            } else if ((opcion === "piedra" && opcionComputadora === "tijera")
-                || (opcion === "papel" && opcionComputadora === "piedra")
-                || (opcion === "tijera" && opcionComputadora === "papel")) {
-                setResultado("Ganaste")
-                vidasComputador[PuntosComputadora] = "💔"
-                console.log(vidasComputador)
-                setVidasComputador(vidasComputador)
+        //actualizamos la seleccion del jugador
+        setJuegadorOpcion(opcion)
+        setPartidaActual(PartidaActual + 1)
+        //Generamos aleatoriamente una Opcion para la  computadora
+        const randomOpcionCompuradora = Math.floor(Math.random() * 3)
+        //Pasamos la opcion 
+        const opcionComputadora = opciones[randomOpcionCompuradora]
+        //actualizamos el estado de la computadora
+        setComputadorOpcion(opcionComputadora)
 
-                setPuntosComputadora(PuntosComputadora + 1)
-            } else {
-                setResultado("Perdiste")
-                vidasJugador[PuntosJugador] = "💔"
-                console.log(vidasComputador)
-                setVidasJugador(vidasJugador)
+        //Validamos el juego actual
+        if (opcion === opcionComputadora) {
+            setResultado("Empate")
+        } else if ((opcion === "piedra" && opcionComputadora === "tijera")
+            || (opcion === "papel" && opcionComputadora === "piedra")
+            || (opcion === "tijera" && opcionComputadora === "papel")) {
+            setResultado("Ganaste")
+            vidasComputador[PuntosComputadora] = "💔"
+            console.log(vidasComputador)
+            setVidasComputador(vidasComputador)
 
-                setPuntosJugador(PuntosJugador + 1)
-            }
-            setPartidaActual(PartidaActual + 1)
-           
-
+            setPuntosComputadora(PuntosComputadora + 1)
+            validaGanador()
         } else {
+            setResultado("Perdiste")
+            vidasJugador[PuntosJugador] = "💔"
+            console.log(vidasComputador)
+            setVidasJugador(vidasJugador)
+
+            setPuntosJugador(PuntosJugador + 1)
             validaGanador()
         }
-        
+        setPartidaActual(PartidaActual + 1)
+
+
+
+
     }
 
 
+    const onReiniciar = () => {
+        setVidasJugador(['💖', '💖', '💖'])
+        setVidasComputador(['💖', '💖', '💖'])
+        setPartidaActual(0)
+        setPuntosJugador(0)
+        setPuntosComputadora(0)
+        setJuegadorOpcion(null)
+        setComputadorOpcion(null)
+        setResultado(null)
+        setFinJuego(false)
+    }
 
 
     const validaGanador = () => {
 
-        if (vidasJugador.filter((item) => item == "💔").length < vidasComputador.filter((item) => item == "💔").length ) {
-            alert('Gana Jugador')
-        } else if (vidasJugador.filter((item) => item == "💔").length == vidasComputador.filter((item) => item == "💔").length ) {
-            alert('Empate')
-        } else {
-            alert('Gana Computador')
+        if (vidasJugador.filter((item) => item == "💔").length === 3) {
+            alert('Fin del juego')
+            setResultado("Gana Computadora")
+            setFinJuego(true)
+        } else if (vidasComputador.filter((item) => item == "💔").length === 3) {
+            alert('Fin del juego')
+            setResultado("Gana Jugador")
+            setFinJuego(true)
         }
 
     }
@@ -98,20 +114,62 @@ setPartidaActual(PartidaActual + 1)
                 </div>
             </div>
             <div>
-                <h1>JUEGUEMOS PIEDRA, PAPEL, O TIJERA</h1>
-                <div>
-                    <button onClick={() => onOpcionCheck("piedra")}>PIEDRA</button>
+                <h1>JUGUEMOS PIEDRA, PAPEL, O TIJERA</h1>
+                <h3>Selecciona una opcion</h3>
+                <div className="contenedor-opciones">
+
+                    <div className={`item animate__animated animate__bounce ${FinJuego ? 'btn-enabled' : ''}`} onClick={() => onOpcionCheck("piedra")}>
+                        <p>Piedra</p>
+                        <img src={piedra} alt="PIEDRA" width="90" />
+                    </div>
+
+
+                    <div className={`item animate__animated animate__bounce ${FinJuego ? 'btn-enabled' : ''}`} onClick={() => onOpcionCheck("papel")}>
+                        <p>PAPEL</p>
+                        <img src={papel} alt="PAPEL" width="90" />
+                    </div>
+
+
+
+                    <div className={`item animate__animated animate__bounce ${FinJuego ? 'btn-enabled' : ''}`} onClick={() => onOpcionCheck("tijera")}>
+                        <p>TIJERA</p>
+                        <img src={tijera} alt="TIJERA" width="90" />
+                    </div>
+
                 </div>
-                <div>
-                    <button onClick={() => onOpcionCheck("papel")}>PAPEL</button>
-                </div>
-                <div>
-                    <button onClick={() => onOpcionCheck("tijera")}>TIJERA</button>
-                </div>
-                <h2>Resultado</h2>
-                <h3>Seleccionaste: {jugadorOpcion}</h3>
-                <h3>La computadora eligio: {computadorOpcion}</h3>
-                <p>{resultado}</p>
+
+                {
+                    !FinJuego &&
+
+                    <>
+                        <div className="contendor-resultado">
+                            <div className={`animate__animated animate__fadeIn ${jugadorOpcion ? 'visible' : 'hidden'}`}>
+                                <h3>Seleccionaste: {jugadorOpcion}</h3>
+                            </div>
+                            <div className={`animate__animated animate__fadeIn ${computadorOpcion ? 'visible' : 'hidden'}`}>
+                                <h3>La computadora eligio: {computadorOpcion}</h3>
+                            </div>
+                        </div>
+                        <div className={` resultado ${resultado ? 'visible' : 'hidden'}`}>
+                            <h2>Resultado</h2>
+                            <div >{resultado}</div>
+                        </div>
+
+                    </>
+                }
+
+
+                {
+
+                    FinJuego &&
+                    <>
+
+                        <button className="btn" onClick={onReiniciar}>REINICIAR</button>
+                        <div className={`${resultado ? 'visible' : 'hidden'}`}>{resultado}</div>
+                    </>
+
+                }
+
             </div>
         </>
     )
